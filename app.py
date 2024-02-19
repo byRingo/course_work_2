2579ad91cbbee40b022a272ccc613924498145c9
Eugene Musihin <egmusikhin@edu.hse.ru>
2024-02-19 21:38:46 +0500

    b

--- app.py
+++ app.py
@@ -5,40 +5,48 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 def db_conn():
-    conn = psycopg2.connect(database = "postgres",host ="localhost", user ="postgres", password ="01031979", port = "5432")
+    conn = psycopg2.connect(database = "postgres",host ="localhost", user ="postgres", password ="123", port = "5432")
     return conn
 
 @app.route('/')
 def index():
     conn = db_conn()
     cur = conn.cursor()
-    cur.execute('''SELECT * FROM testing''')
+    cur.execute('''SELECT * FROM person''')
     data = cur.fetchall()
     cur.close()
     conn.close()
-    return render_template('index_test.html', data = data)
+    return render_template('index.html', data = data)
 
 @app.route('/create', methods=['POST'])
 def create():
     conn = db_conn()
     cur = conn.cursor()
     name = request.form['name']
-    cur.execute('''INSERT INTO testing (name) VALUES(%s)''',(name,))
+    last_name = request.form['last-name']
+    email = request.form['email']
+    phone_number = request.form['phone-number']
+    password = request.form['password']
+    cur.execute('''INSERT INTO person (name, last-name, email, phone-number, password) VALUES(%s, %s, %s, %s, %s)''',(name,last_name,email, phone_number, password ))
     conn.commit()
     cur.close()
     conn.close()
-    return redirect(url_for('index_test'))
+    return redirect(url_for('index'))
 
 @app.route('/update', methods = ['POST'])
 def update():
     conn = db_conn()
     cur = conn.cursor()
     name = request.form['name']
-
-    cur.execute('''UPDATE testing SET name =%s WHERE id=%s''',(name,id))
+    last_name = request.form['last-name']
+    email = request.form['email']
+    phone_number = request.form['phone-number']
+    password = request.form['password']
+    id = request.form['id']
+    cur.execute('''UPDATE person SET name =%s, last-name =%s, email=%s, phone-number=%s, password=%s WHERE id=%s''',(name,last_name, email, phone_number, password, id))
 
     conn.commit()
-    return redirect(url_for('index_test'))
+    return redirect(url_for('index'))
 
 
 @app.route('/delete', methods = ['POST'])
@@ -47,7 +55,8 @@ def delete():
     cur = conn.cursor()
     id = request.form['id']
 
-    cur.execute('''DELETE FROM testing WHERE id=%s''',(id,))
+    cur.execute('''DELETE FROM person WHERE id=%s''',(id,))
 
     conn.commit()
-    return redirect(url_for('index_test'))
+    return redirect(url_for('index'))
+app.run(debug = True)
--- init_db.py
+++ init_db.py
@@ -3,8 +3,8 @@ import psycopg2
 conn = psycopg2.connect(database = "postgres",host ="localhost", user ="postgres", password ="01031979", port = "5432")
 cur = conn.cursor()
 
-cur.execute('''CREATE TABLE IF NOT EXISTS testing (id serial PRIMARY KEY, name varchar(100))''')
-cur.execute('''INSERT INTO testing (name) VALUES ('test')''')
+cur.execute('''CREATE TABLE IF NOT EXISTS person (id serial PRIMARY KEY, name varchar(100), last-name varchar(100), email varchar(100), phone-number varchar(12), password varchar(100))''')
+cur.execute('''INSERT INTO person (name, last-name, email, phone-number, password) VALUES ('name, last-name, email, phone-number, password') ''')
 
 conn.commit()
 cur.close()
